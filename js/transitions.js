cf10e0d3083b7099d3f181c6dbfe4d0811631ce7
ProfAsim <159194865+ProfAsim@users.noreply.github.com>
2025-03-05 00:11:14 -0500

    Update transitions.js

--- js/transitions.js
+++ js/transitions.js
@@ -35,6 +35,71 @@ const Scene2 = {
         text.setOrigin(0.5);
 
         this.input.keyboard.on('keydown-SPACE', function () {
+            this.scene.start('Scene3');
+        }, this);
+    },
+
+    update: function () {}
+};
+
+const Scene3 = {
+    preload: function () {
+        this.load.image('goldenfield', 'assets/goldenfield.webp');
+    },
+
+    create: function () {
+        this.add.image(0, 0, 'goldenfield').setOrigin(0, 0);
+
+        const text = this.add.text(400, 300, 'Scene 3 - Press Space to Go Back', {
+            fontSize: '24px',
+            fill: '#fff'
+        });
+        text.setOrigin(0.5);
+
+        this.input.keyboard.on('keydown-SPACE', function () {
+            this.scene.start('Scene4');
+        }, this);
+    },
+
+    update: function () {}
+};
+
+const Scene4 = {
+    preload: function () {
+        this.load.image('bluesky', 'assets/bluesky.webp');
+    },
+
+    create: function () {
+        this.add.image(0, 0, 'bluesky').setOrigin(0, 0);
+
+        const text = this.add.text(400, 300, 'Scene 4 - Press Space to Go Back', {
+            fontSize: '24px',
+            fill: '#fff'
+        });
+        text.setOrigin(0.5);
+
+        this.input.keyboard.on('keydown-SPACE', function () {
+            this.scene.start('Scene5');
+        }, this);
+    },
+
+    update: function () {}
+};
+const Scene5 = {
+    preload: function () {
+        this.load.image('goldenlight', 'assets/goldenlight.webp');
+    },
+
+    create: function () {
+        this.add.image(0, 0, 'goldenlight').setOrigin(0, 0);
+
+        const text = this.add.text(400, 300, 'Scene 5 - Press Space to Go Back', {
+            fontSize: '24px',
+            fill: '#fff'
+        });
+        text.setOrigin(0.5);
+
+        this.input.keyboard.on('keydown-SPACE', function () {
             this.scene.start('default');
         }, this);
     },
@@ -56,6 +121,9 @@ const game = new Phaser.Game(config);
 //  Add scenes to the game
 game.scene.add('default', Scene1);
 game.scene.add('Scene2', Scene2);
+game.scene.add('Scene3', Scene3);
+game.scene.add('Scene4', Scene4);
+game.scene.add('Scene5', Scene5);
 
 //  Start the first scene
 game.scene.start('default');
